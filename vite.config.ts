a068588468c7c5496972ef7b5649f05effaa021d
Rakesh <rakesh@skills-agency.com>
2022-10-13 15:00:01 +0530

    Working Bootstrap template

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  plugins: [svelte()],
+  server: {
+    port: 6500,
+    hmr: true
+  }
 })
